deeb64d335a6c23e6518a02804fd52099b0108af
Jonahtas75 <144528052+Jonahtas75@users.noreply.github.com>
2023-09-09 13:06:17 +0200

    Update main.c
    Volledig automatisch systeem. 
    
    -Brug checkt of er een boot is en er geen wegverkeer is -->
    -Brug gaat open --> 
    -Brug checkt of de boot voorbij is --> 
    -Brug gaat dicht -->
    -Repeat

--- main.c
+++ main.c
@@ -1,29 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dos.h>
 
 int main()
 {
-    int signaalBoot = 1;
-    int wegverkeer = 0;
+    int signaalBoot;
+    int wegverkeer;
     int motorBrug = 0;
-    // motorBrug = 0 -> brug staat stil
-    // motorBrug = 1 -> brug gaat omhoog
-    // motorBrug = -1 -> brug gaat omlaag
+        // motorBrug = 0 -> Brug staat stil
+        // motorBrug = 1 -> Brug gaat omhoog
+        // motorBrug = -1 -> Brug gaat omlaag
+    int sensorOpen = 0;
+        // sensor die checkt of de brug volledig open is
+    int sensorDicht = 1;
+        // sensor die checkt of de brug volledig dicht is
+    int hoogte = 0;
+    int brugAan = 1;
 
+    void brugChecker()
+    {
+        while(sensorDicht) // Als brug dicht is
+        {
+            // Check voor boten en wegverkeer
+            if(signaalBoot && !wegverkeer)
+            {
+                sensorDicht = 0;
+                motorBrug = 1;
+                break;
+            }
+
+
+            printf("dicht\n");
+            sleep(1);
+            printf("Boot signaal?\n");
+            scanf("%d",&signaalBoot);
+            printf("Wegverkeer?\n");
+            scanf("%d",&wegverkeer);
+        }
+
+        while(motorBrug == 1) // Als brug aan het openen is
+        {
+            // Stuur motor aan
+
+            printf("openen\n");
+            hoogte++;
+            sleep(1);
+            if(hoogte == 5)
+            {
+                motorBrug = 0;
+                sensorOpen = 1;
+                break;
+            }
+        }
+
+
+        while(sensorOpen) // Als brug open is
+        {
+            // Check of boot voorbij is
+
+            printf("open\n");
+            sleep(1);
+            printf("Boot signaal?\n");
+            scanf("%d",&signaalBoot);
+            if(!signaalBoot)
+            {
+                sensorOpen = 0;
+                motorBrug = -1;
+                break;
+            }
+
+        }
+
+        while(motorBrug == -1) // Als brug aan het sluiten is
+        {
+            // Stuur motor aan
+
+            printf("sluiten\n");
+            hoogte--;
+            sleep(1);
+            if(hoogte == 0)
+            {
+                motorBrug = 0;
+                sensorDicht = 1;
+                break;
+            }
+        }
+    }
+
+//-------------------------------------------------------------------------------------//
     printf("Boot signaal?\n");
     scanf("%d",&signaalBoot);
     printf("Wegverkeer?\n");
     scanf("%d",&wegverkeer);
 
-    if(signaalBoot && !wegverkeer)
+    while(brugAan)
     {
-        motorBrug = 1;
+       brugChecker();
     }
 
-    while(motorBrug == 1)
-    {
-        printf("openen\n");
-
-    }
     return 0;
 }
